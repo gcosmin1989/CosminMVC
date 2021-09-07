ef3fc2ca01bc5a88c515d803b0bb4520e4bcbe25
gcosmin1989 <gherghina.cosmin@yahoo.com>
2021-09-07 15:46:55 +0300

    Fisierul de Config si Autoloader-ul

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,11 @@
 <?php
-//Incarcare librarii
 
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+//Incarcare config si va arata ruta
+require_once 'config/config.php';
+
+//Autoload Librariile din Core
+
+spl_autoload_register(function ($className){
+    //Fisierele trebuie sa aibe acelasi nume precum clasele
+    require_once 'libraries/' . $className .'.php';
+});
---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//app root
+define('APPROOT',dirname(dirname(__FILE__)));
+
+//url root
+define('URLROOT','http://localhost/cosminmvc');
+
+//numele site-ului
+define('SITENAME', 'CosminMVC');
