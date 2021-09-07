4e8a003d0524624ffcad8af15fed7501b7c2eb53
gcosmin1989 <gherghina.cosmin@yahoo.com>
2021-09-07 15:10:16 +0300

    Loading Controller From URL

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -3,17 +3,42 @@
 //Creaza URL-uri si incarca Core Controller
 //URL Format -/controller/method/params
 
-class Core{
-    protected $currentController ='Pages';
+class Core
+{
+    protected $currentController = 'Pages';
     protected $currentMethod = 'index';
-    protected $params =[];
+    protected $params = [];
 
-    public function __construct(){
-        $this->getUrl();
+    public function __construct()
+    {
+        //print_r($this->getUrl());
+        $url = $this->getUrl();
+        //Ne uitam in Controller pentru prima valoare din array, definim calea ca si cum am fii in index.php
+
+        if ((!empty($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php') &&isset($url[0]))) {
+//verifica daca exista, si setam ca controllerul principal
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+        //solicitare controller
+        require_once '../app/controllers/'. $this->currentController .'.php';
+
+        //Instatiere clasa controller
+        $this->currentController = new $this->currentController;
+
     }
 
-    public function getUrl(){
+    public function getUrl()
+    {
         //preluare parametrii din URL
-        echo $_GET['url'];
+        if (isset($_GET['url'])) {
+            //Eliminare / din Url
+            $url = rtrim($_GET['url'], '/');
+            //Curatare url de charactere nepermise
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            //Punerea intr-un array valoarea dupa fiecare /
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
